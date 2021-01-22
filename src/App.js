427ae7ab2e14da42909e5244b2bc76ec6b428fa0
Arcadiusz <Arek94@onet.com.pl>
2021-01-22 21:30:53 +0100

    basic Player and Song components added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+
+import Player from "./components/Player";
+import Song from "./components/Song";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <Song />
+            <Player />
+        </div>
+    );
 }
 
 export default App;
