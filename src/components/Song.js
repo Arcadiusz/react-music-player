1b97cdf87ffa349f668f647f3738ef46b88dbbcf
Arcadiusz <Arek94@onet.com.pl>
2021-01-23 12:00:27 +0100

    basic styling added to the app

--- src/components/Song.js
+++ src/components/Song.js
@@ -2,7 +2,7 @@ import React from "react";
 import Plater from "./Player";
 const Song = () => {
     return (
-        <div className="song-container">
+        <div class="song-container">
             <h1>picture</h1>
             <h1>Song Name</h1>
             <h1>Artist</h1>
